020984dbababebce9de61b311420e28f7b366da5
Aaron Miller <miller_aaron@hotmail.com>
2022-06-26 20:25:42 -0700

    adding files

---
+++ Simple_Map/static/js/logic.js
@@ -0,0 +1,16 @@
+// add console.log to check if code is working
+
+console.log("working");
+
+// map object
+
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: map_key
+});
+
+// add 'graymap' tile layer to the map.
+streets.addTo(map);
