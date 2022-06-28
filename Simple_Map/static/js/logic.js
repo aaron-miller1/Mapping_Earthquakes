2ae893da21ebc296bf67e109d6c947eb9b30f78b
Aaron Miller <miller_aaron@hotmail.com>
2022-06-27 18:14:37 -0700

    updated + new files

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -15,6 +15,19 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // add 'graymap' tile layer to the map.
 streets.addTo(map);
 
-let marker = L.circleMarker([34.0522, -118.2347], {radius: 300, color: "black", fillColor: "ffffa1"}).addTo(map);
+let marker = L.circle([34.0522, -118.2347], {radius: 100, color: "black", fillColor: "ffffa1"}).addTo(map);
+
+// Get cities
+
+let cityData = cities;
 
 
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: "orange"
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+})
