fc8763b1a95b91e8e919818088718ff0bd468210
Aaron Miller <miller_aaron@hotmail.com>
2022-06-27 17:23:10 -0700

    dark map

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -4,9 +4,9 @@ console.log("working");
 
 // map object
 
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([40.7, -94.5], 14);
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: map_key
@@ -14,3 +14,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // add 'graymap' tile layer to the map.
 streets.addTo(map);
+
+let marker = L.circleMarker([34.0522, -118.2347], {radius: 300, color: "black", fillColor: "ffffa1"}).addTo(map);
+
+
