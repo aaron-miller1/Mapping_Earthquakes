e24cb0a9233955df3394a2b8f71b9fa93ad7539c
Aaron Miller <miller_aaron@hotmail.com>
2022-06-30 19:39:15 -0700

    updates

---
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -0,0 +1,65 @@
+
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: map_key
+});
+
+// Dark Map
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: map_key
+});
+
+// Base layer
+
+let baseMaps = {
+    "Streets": streets,
+    "Satellite Streets": satelliteStreets
+};
+
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+});
+
+L.control.layers(baseMaps).addTo(map);
+
+// Retrieve Earthquake data
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+    L.geoJSON(data, {
+
+        pointToLayer: function(feature, latlng) {
+            console.log(data);
+
+            return L.circleMarker(latlng);
+        },
+    style: styleInfo
+    }).addTo(map);
+});
+
+// STYLE
+
+function getRadius(magnitude) {
+    if (magnitude === 0) {
+        return 1;
+    }
+
+    return magnitude * 4;
+}
+
+function styleInfo(feature) {
+    return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: "#ffae42",
+        color: "#574fff",
+        radius: getRadius(),
+        stroke: true,
+        weight: 0.5
+    };
+}
